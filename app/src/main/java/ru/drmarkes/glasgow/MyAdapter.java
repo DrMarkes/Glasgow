0360a5216eb82b065c775841aabfc14935dcf9ce
DrMarkes <markovandrew@yandex.ru>
2016-02-22 15:08:33 +0700

    Initial commit

---
+++ app/src/main/java/ru/drmarkes/glasgow/MyAdapter.java
@@ -0,0 +1,24 @@
+package ru.drmarkes.glasgow;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Андрей on 22.02.2016.
+ */
+public class MyAdapter extends FragmentPagerAdapter {
+    public MyAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/ru/drmarkes/glasgow/PageFragment.java
@@ -0,0 +1,12 @@
+package ru.drmarkes.glasgow;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Андрей on 22.02.2016.
+ */
+public class PageFragment extends Fragment {
+    public static Fragment newInstance() {
+
+    }
+}
